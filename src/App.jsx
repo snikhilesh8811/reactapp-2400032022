d040173baadce94fa4fd94b7fd56eb28926758af
nikhilesh <nikhilesh6757@gmail.com>
2026-08-28 08:45:23 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './style.css'
+
+function App() {
+  return (
+    <main className="welcome-page">
+      <h1>Welcome to my React app</h1>
+    </main>
+  )
+}
+
+export default App
